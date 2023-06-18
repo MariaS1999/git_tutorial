ea4e8f104cec19c62c98772118c225d86c3834b8
Masha_S <masha.sytova99@gmail.com>
2023-06-18 12:28:53 +0300

    switch case 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 "use strict";
 
-let i = 0;
-while (i < 3) {
-  alert( `number ${i}!` );
-  i++;
+/* switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+} */
+
+let browser = prompt("Введите браузер");
+
+if (browser === "Edge") {
+  alert( "You've got the Edge!" );
+} else if ((browser === 'Chrome') || 
+  (browser === 'Firefox' ) || 
+  (browser === 'Opera') || 
+  (browser === 'Safari')) {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
 }
