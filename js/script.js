7026ce5282676c549096511f11f89402388ee101
Masha_S <masha.sytova99@gmail.com>
2023-06-18 12:33:03 +0300

    switch case 2

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 "use strict";
 
-/* switch (browser) {
-  case 'Edge':
-    alert( "You've got the Edge!" );
+const number = +prompt('Введите число между 0 и 3', '');
+
+/* if (number === 0) {
+  alert('Вы ввели число 0');
+}
+
+if (number === 1) {
+  alert('Вы ввели число 1');
+}
+
+if (number === 2 || number === 3) {
+  alert('Вы ввели число 2, а может и 3');
+} */
+
+switch (number) {
+  case 0:
+    alert('Вы ввели число 0');
     break;
 
-  case 'Chrome':
-  case 'Firefox':
-  case 'Safari':
-  case 'Opera':
-    alert( 'Okay we support these browsers too' );
+  case 1:
+    alert('Вы ввели число 1');
+    break;
+
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3');
     break;
 
   default:
-    alert( 'We hope that this page looks ok!' );
-} */
-
-let browser = prompt("Введите браузер");
-
-if (browser === "Edge") {
-  alert( "You've got the Edge!" );
-} else if ((browser === 'Chrome') || 
-  (browser === 'Firefox' ) || 
-  (browser === 'Opera') || 
-  (browser === 'Safari')) {
-  alert( 'Okay we support these browsers too' );
-} else {
-  alert( 'We hope that this page looks ok!' );
+    break;
 }
