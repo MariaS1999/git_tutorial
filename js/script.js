9765b60a06bd841774015bba7693ab85e79c9339
Masha_S <masha.sytova99@gmail.com>
2023-06-18 11:37:14 +0300

    Homework for and if 1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,7 @@
 "use strict";
 
-let userName = prompt("Введите имя");
-let password;
-
-if (userName === "Админ") { 
-    password = prompt("Пароль");
-    password === "Я Главный" ?
-        alert("Здравствуйте") :
-        password === null ?
-            alert("Отменено") :
-            alert("Неверный пароль") 
-} else if (userName === null) {
-    alert("Отменено")
-} else {
-    alert("Я вас не знаю")
-}
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+      alert( i );
+    }
+  }
