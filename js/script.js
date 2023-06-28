a6f508b64693a010660b91b5361e0ad0068acffa
Masha_S <masha.sytova99@gmail.com>
2023-06-28 19:19:31 +0300

    typeof

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 "use strict";
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == 'number') {
+      obj[key] *= 2;
+    }
+  }
 }
 
-let sum = 0;
-for (let key in salaries) {
-  sum += salaries[key];
-}
-
-alert(sum);
+multiplyNumeric(menu);
+  
+  alert( "menu width=" + menu.width + " height=" + menu.height + " title=" + menu.title );
