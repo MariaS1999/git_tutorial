37cf2d88ce3cef42ff3d5db55302e40378ee6804
Masha_S <masha.sytova99@gmail.com>
2023-06-28 21:55:30 +0300

    objects function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 "use strict";
 
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
+let input = prompt('Number'); // 0 .. 999 245
+console.log(getObject(input));
 
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] == 'number') {
-      obj[key] *= 2;
+function checkType(str) {
+  if (Boolean(Number(str))) {
+    return Number(str);
+  } else {
+    alert("Вы ввели не число");
+  }
+}
+
+function checkValue(num) {
+  if (checkType(num)) {
+    if ((num >= 0) && (num < 1000)) {
+      return num;
+    } else {
+      alert("Неверное число");
     }
   }
 }
 
-multiplyNumeric(menu);
-  
-  alert( "menu width=" + menu.width + " height=" + menu.height + " title=" + menu.title );
+function getObject(num) {
+  let objectNum = {};
+  let rem, div, count = 1;
+  div = checkValue(num);
+  if (div) {
+    while (div != 0) {
+      rem = div % 10;
+      if (count === 1) {
+        objectNum["единиц"] = rem;
+      } else if (count === 2) {
+        objectNum["десятков"] = rem;
+      } else {
+        objectNum["сотен"] = rem;
+      }
+      div = Math.trunc(div / 10);
+      count++;
+    } 
+    return objectNum;
+  }
+}
