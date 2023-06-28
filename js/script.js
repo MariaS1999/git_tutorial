d07b6b9617b85148618b107d539c2fbff1622174
Masha_S <masha.sytova99@gmail.com>
2023-06-28 19:13:34 +0300

    object + for

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 "use strict";
 
-/* Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
-
-Должно работать так: */
-
-let schedule = {};
-alert( isEmpty(schedule) ); // true
-schedule["8:30"] = "get up";
-alert( isEmpty(schedule) ); // false 
-
-function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
 }
 
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+alert(sum);
